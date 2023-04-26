1f0993c0fb496807871a42d7e2b1769ef6702957
Sergii Kostanets <sergiikostanets@gmail.com>
2023-04-26 12:54:31 +0100

    change templates folder

--- django_united/settings.py
+++ django_united/settings.py
@@ -68,7 +68,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
-        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
