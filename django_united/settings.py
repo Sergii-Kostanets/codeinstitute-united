beca9abb852f3fb70b529b4e1a9e1e8917df6aaf
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-05 11:59:23 +0100

    authorization completed

--- django_united/settings.py
+++ django_united/settings.py
@@ -21,7 +21,7 @@ development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,8 +79,8 @@ ROOT_URLCONF = 'django_united.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [TEMPLATES_DIR],
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
