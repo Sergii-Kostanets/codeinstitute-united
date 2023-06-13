708ffb6c0a6194354e9ffbe119ba627aa91c5142
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-13 17:04:15 +0100

    add game creation

--- connect/urls.py
+++ connect/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GameList.as_view(), name='game_list'),
+    path('game_create', views.GameCreate.as_view(), name='game_create'),
 ]
--- connect/views.py
+++ connect/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import generic, View
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import GameForm
 
 from .models import Game
 
@@ -10,3 +14,14 @@ class GameList(generic.ListView):
     queryset = Game.objects.filter(status=1).order_by('-created_on')
     template_name = 'connect/game_list.html'
     paginate_by = 5
+
+
+class GameCreate(LoginRequiredMixin, CreateView):
+    model = Game
+    form_class = GameForm
+    template_name = 'connect/game_create.html'
+    success_url = reverse_lazy('game_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
