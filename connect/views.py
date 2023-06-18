c309ee2b88c117315290e948fdb669aed7f08c09
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-18 17:46:33 +0100

    add search to ma games page

--- connect/views.py
+++ connect/views.py
@@ -6,9 +6,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import GameForm
 from .models import Game
-
 from django.http import JsonResponse
-
 from django.db.models import Q
 
 
@@ -126,4 +124,15 @@ class GamesOfUser(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        return Game.objects.filter(author=self.request.user).order_by('-created_on')
+        search_query = self.request.GET.get('search')
+        queryset = Game.objects.filter(author=self.request.user).order_by('-created_on')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(platform__icontains=search_query) |
+                Q(excerpt__icontains=search_query) |
+                Q(content__icontains=search_query)
+            )
+
+        return queryset
