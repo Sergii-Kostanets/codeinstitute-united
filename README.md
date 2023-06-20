8f8548a20a70ad50e3200ed9717c51f3d905516b
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-20 21:08:18 +0100

    add BeautifulSoup for SummerNote validation

--- README.md
+++ README.md
@@ -6,10 +6,19 @@ python3 manage.py runserver
 ```
 ```
 python3 manage.py makemigrations --dry-run
+```
+```
 python3 manage.py showmigrations
+```
+```
 python3 manage.py makemigrations
+```
+```
 python3 manage.py migrate --plan
+```
+```
 python3 manage.py migrate
 ```
-
+```
 python3 manage.py collectstatic --no-input
+```
--- connect/forms.py
+++ connect/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Game
 from django_summernote.widgets import SummernoteWidget
+from bs4 import BeautifulSoup
 
 
 class GameForm(forms.ModelForm):
@@ -9,3 +10,13 @@ class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ('title', 'slug', 'platform', 'excerpt', 'content', 'featured_image')
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if content:
+            # Remove HTML tags and check if the resulting content is empty
+            stripped_content = ''.join(BeautifulSoup(content).findAll(text=True))
+            if not stripped_content.strip():
+                raise forms.ValidationError("Content field is required")
+        return content
+
