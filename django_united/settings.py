52428eeab90dc0544fdc0448828d53a76c58ce1e
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-21 22:55:21 +0100

    add domain

--- django_united/settings.py
+++ django_united/settings.py
@@ -40,7 +40,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 if development:
     ALLOWED_HOSTS = ['127.0.0.1']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), os.environ.get('NAME_DOMAIN')]
 
 # Application definition
 
