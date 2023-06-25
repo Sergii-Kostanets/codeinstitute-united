e9c3a2a26d529fe2848fd66d4e90293daa79f057
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-25 23:15:22 +0100

    add validation for content field

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from .models import Post
 from .models import Comment
 from django import forms
 from django_summernote.widgets import SummernoteWidget
+from bs4 import BeautifulSoup
 
 
 class PostForm(forms.ModelForm):
@@ -9,6 +10,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'excerpt', 'content', 'featured_image']
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if content:
+            # Remove HTML tags and check if the resulting content is empty
+            stripped_content = ''.join(BeautifulSoup(content).findAll(text=True))
+            if not stripped_content.strip():
+                raise forms.ValidationError("Content field is required")
+        return content
 
 
 class CommentForm(forms.ModelForm):
