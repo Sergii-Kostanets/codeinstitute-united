701d29cc86457d21b9d4ccf09cdc2ef3d7553112
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-25 23:13:47 +0100

    add ummernote widget

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,11 @@
 from .models import Post
 from .models import Comment
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=SummernoteWidget())
     class Meta:
         model = Post
         fields = ['title', 'excerpt', 'content', 'featured_image']
