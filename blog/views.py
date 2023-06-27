a99f849cbe6a28dc3feea61c9ea10d019a17b7e2
Sergii Kostanets <sergiikostanets@gmail.com>
2023-06-27 17:14:24 +0100

    fix bug with comments

--- blog/views.py
+++ blog/views.py
@@ -48,15 +48,15 @@ class PostDetail(View):
             if post.status == 0 and post.author != request.user:
                 raise PermissionDenied
 
-        self.post = post
+        self.blog_post = post
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, slug, *args, **kwargs):
-        post = self.post
+        post = self.blog_post
 
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
 
         return render(
@@ -72,11 +72,10 @@ class PostDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        post = self.blog_post
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
         
         comment_form = CommentForm(data=request.POST)
@@ -99,7 +98,7 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "liked": liked,
-                "comment_form": CommentForm(),
+                "comment_form": comment_form,
             },
         )
 
